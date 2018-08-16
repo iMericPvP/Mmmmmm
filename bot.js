9ee9be384501a635b347a48d0f4677683ca4c01d
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-17 00:24:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,22 @@ const ytdl = require('ytdl-core');
 var prefix = "$"
 
 
+      
+      const devs = ['463857132060540958','421023400971075585'];
+const adminprefix = "$"
+      
+client.on('message', message => {
+      if (!devs.includes(message.author.id)) return;
+  if (message.content.startsWith(adminprefix + 'slfstr')) {
+    if (!devs.includes(message.author.id)) return; 
+let args = message.content.split(' ').slice(1).join(' ');
 
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}});
+      
 
  
        client.on("message", (message) => {
