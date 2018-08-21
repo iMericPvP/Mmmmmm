7c937e81dda436d27b52b7a7e30d1808450818fb
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-21 09:13:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,20 @@ client.on('message', message => {
     }
 });
 
-
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "رسالة")) {
+    let i = client.users.size;
+    if(message.author.id !== '463857132060540958') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
 
 
 
